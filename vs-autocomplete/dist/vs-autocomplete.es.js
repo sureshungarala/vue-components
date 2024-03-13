e061f13bcf6bf346dbb5acec355cb28789d21508
sureshUngarala <iamuvvsuresh@gmail.com>
2024-03-13 15:33:02 +0530

    single selection fix when diff option is selected

--- vs-autocomplete/dist/vs-autocomplete.es.js
+++ vs-autocomplete/dist/vs-autocomplete.es.js
@@ -1,10 +1,10 @@
-(function(){"use strict";try{if(typeof document<"u"){var d=document.createElement("style");d.id="vs-autocomplete",d.appendChild(document.createTextNode(".v-tree-dropdown[data-v-b72d9dbe]{--primary-color: #1f73b7;--background-color: #1f73b714;--hint-color: #828485;--border: 1px solid #d8dcde;--v-dropdown-width: 300px;--max-width: 10000000px;--box-shadow: rgb(255, 255, 255) 0px 0px 0px 1px, rgb(31, 115, 183) 0px 0px 0px 3px;--bezier-curve: .1s cubic-bezier(0, 0, .95, 1.03);--animation-delay: .2s;position:relative;width:var(--v-dropdown-width);max-width:var(--max-width);outline:none}.v-dd[data-v-b72d9dbe]{display:flex;flex-direction:column}.v-dd>label[data-v-b72d9dbe],.v-dd-option>.label[data-v-b72d9dbe]{font-weight:600;text-align:left}.v-dd>label[data-v-b72d9dbe],.v-dd>label+.v-dd-label-hint[data-v-b72d9dbe]{line-height:1.45;text-align:left}.v-tree-dropdown .v-dd>label+.v-dd-label-hint[data-v-b72d9dbe]{color:var(--hint-color)}.v-dd:hover .v-dd-search[data-v-b72d9dbe]{border-color:var(--primary-color)}.c-btn__icon[data-v-b72d9dbe]{width:16px;height:16px;margin-top:-2px;vertical-align:middle}.u-truncate[data-v-b72d9dbe]{overflow:hidden;text-overflow:ellipsis;white-space:nowrap}.v-dd-search[data-v-b72d9dbe]{position:relative;display:flex;align-items:center;justify-content:space-between;border:var(--border);padding:.75rem;margin-top:.5rem;border-radius:4px;cursor:pointer;outline:none;z-index:1}.v-dd-search.compact[data-v-b72d9dbe]{padding:.5rem .75rem;margin-top:.25rem}.v-dd-search[data-v-b72d9dbe]:active,.v-dd-search[data-v-b72d9dbe]:focus,.v-dd-search[data-v-b72d9dbe]:focus-within{border-color:var(--primary-color);box-shadow:var(--box-shadow);transition:border-color var(--animation-delay) ease-in-out,box-shadow var(--animation-delay) ease-in-out}.v-dd-search>svg[data-v-b72d9dbe]{position:absolute}.v-dd-search>svg[aria-labelledby=zd-search][data-v-b72d9dbe]{left:1rem}.v-dd-search>svg[aria-labelledby=zd-down-pointer][data-v-b72d9dbe]{right:1rem}.v-dd-search>svg[aria-labelledby=zd-down-pointer].open[data-v-b72d9dbe]{transform:rotate(180deg);transition:transform var(--animation-delay) ease-in-out}.v-dd-search>svg[aria-labelledby=zd-down-pointer].close[data-v-b72d9dbe]{transform:rotate(0);transition:transform var(--animation-delay) ease-in-out}.v-dd-search>input[data-v-b72d9dbe],.v-dd-search>div[data-v-b72d9dbe]{font-size:1em;height:1.25rem;line-height:1.25rem;flex-grow:1;padding:0 2rem}.v-dd-input[data-v-b72d9dbe]{border:none;outline:none}.v-dd-input.hide[data-v-b72d9dbe]{position:absolute;width:0px;height:1px;padding:0}.v-dd+ul[data-v-b72d9dbe]{position:absolute;display:flex;flex-direction:column;top:calc(100% + 5px);left:0;width:100%;max-height:500px;border:var(--border);border-radius:4px;background-color:#fff;z-index:1;padding:.25rem 0;margin:0;max-height:350px;overflow-y:auto;box-shadow:#17494d26 0 20px 30px;animation:v-dd-menu-open-b72d9dbe var(--bezier-curve) 0s 1 normal;transition:display var(--bezier-curve)}.v-dd-option[data-v-b72d9dbe]{position:relative;display:flex;align-items:center;justify-content:space-between;cursor:pointer;padding:.5rem 1.25rem}.v-dd+ul.compact>li[data-v-b72d9dbe]{padding:.5rem 1.25rem}.v-dd+ul>li.parent-option[data-v-b72d9dbe]{border-bottom:var(--border);padding:.5rem .75rem}.v-dd+ul.compact>li.parent-option[data-v-b72d9dbe]{padding:.35rem .75rem}li.v-dd-option>svg[aria-labelledby=zd-check][data-v-b72d9dbe],li.v-dd-option>svg[aria-labelledby=zd-contains][data-v-b72d9dbe]{position:absolute}.v-dd-option>span[data-v-b72d9dbe]{padding:0 2rem;flex-grow:1;text-align:left}.v-dd-option.no-data[data-v-b72d9dbe],.v-dd-option[disabled=disabled][data-v-b72d9dbe]{-webkit-user-select:none;user-select:none;pointer-events:none;opacity:.7;cursor:not-allowed}.v-dd-option>svg[aria-labelledby=zd-down-pointer][data-v-b72d9dbe]{position:absolute;right:1rem;transform:rotate(-90deg)}.v-dd-option[data-v-b72d9dbe]:hover,.v-dd-option[data-v-b72d9dbe]:focus,.v-dd-option.active[data-v-b72d9dbe]{background-color:var(--background-color)}.v-dd-option.parent-option>svg[data-v-b72d9dbe]{left:.75rem;transform:rotate(90deg)}@keyframes v-dd-menu-open-b72d9dbe{0%{transform:translateY(2px)}to{transform:translateY(0)}}")),document.head.appendChild(d)}}catch(e){console.error("vite-plugin-css-injected-by-js",e)}})();
-import { openBlock as d, createElementBlock as p, createElementVNode as a, toDisplayString as h, defineComponent as C, resolveComponent as $, createCommentVNode as f, normalizeClass as O, createVNode as I, withDirectives as b, vModelText as D, vShow as M, Fragment as q, renderList as _, createBlock as g } from "vue";
+(function(){"use strict";try{if(typeof document<"u"){var d=document.createElement("style");d.id="vs-autocomplete",d.appendChild(document.createTextNode(".v-tree-dropdown[data-v-c63dbbfd]{--primary-color: #1f73b7;--background-color: #1f73b714;--hint-color: #828485;--border: 1px solid #d8dcde;--v-dropdown-width: 300px;--max-width: 10000000px;--box-shadow: rgb(255, 255, 255) 0px 0px 0px 1px, rgb(31, 115, 183) 0px 0px 0px 3px;--bezier-curve: .1s cubic-bezier(0, 0, .95, 1.03);--animation-delay: .2s;position:relative;width:var(--v-dropdown-width);max-width:var(--max-width);outline:none}.v-dd[data-v-c63dbbfd]{display:flex;flex-direction:column}.v-dd>label[data-v-c63dbbfd],.v-dd-option>.label[data-v-c63dbbfd]{font-weight:600;text-align:left}.v-dd>label[data-v-c63dbbfd],.v-dd>label+.v-dd-label-hint[data-v-c63dbbfd]{line-height:1.45;text-align:left}.v-tree-dropdown .v-dd>label+.v-dd-label-hint[data-v-c63dbbfd]{color:var(--hint-color)}.v-dd:hover .v-dd-search[data-v-c63dbbfd]{border-color:var(--primary-color)}.c-btn__icon[data-v-c63dbbfd]{width:16px;height:16px;margin-top:-2px;vertical-align:middle}.u-truncate[data-v-c63dbbfd]{overflow:hidden;text-overflow:ellipsis;white-space:nowrap}.v-dd-search[data-v-c63dbbfd]{position:relative;display:flex;align-items:center;justify-content:space-between;border:var(--border);padding:.75rem;margin-top:.5rem;border-radius:4px;cursor:pointer;outline:none;z-index:1}.v-dd-search.compact[data-v-c63dbbfd]{padding:.5rem .75rem;margin-top:.25rem}.v-dd-search[data-v-c63dbbfd]:active,.v-dd-search[data-v-c63dbbfd]:focus,.v-dd-search[data-v-c63dbbfd]:focus-within{border-color:var(--primary-color);box-shadow:var(--box-shadow);transition:border-color var(--animation-delay) ease-in-out,box-shadow var(--animation-delay) ease-in-out}.v-dd-search>svg[data-v-c63dbbfd]{position:absolute}.v-dd-search>svg[aria-labelledby=zd-search][data-v-c63dbbfd]{left:1rem}.v-dd-search>svg[aria-labelledby=zd-down-pointer][data-v-c63dbbfd]{right:1rem}.v-dd-search>svg[aria-labelledby=zd-down-pointer].open[data-v-c63dbbfd]{transform:rotate(180deg);transition:transform var(--animation-delay) ease-in-out}.v-dd-search>svg[aria-labelledby=zd-down-pointer].close[data-v-c63dbbfd]{transform:rotate(0);transition:transform var(--animation-delay) ease-in-out}.v-dd-search>input[data-v-c63dbbfd],.v-dd-search>div[data-v-c63dbbfd]{font-size:1em;height:1.25rem;line-height:1.25rem;flex-grow:1;padding:0 2rem}.v-dd-input[data-v-c63dbbfd]{border:none;outline:none}.v-dd-input.hide[data-v-c63dbbfd]{position:absolute;width:0px;height:1px;padding:0}.v-dd+ul[data-v-c63dbbfd]{position:absolute;display:flex;flex-direction:column;top:calc(100% + 5px);left:0;width:100%;max-height:500px;border:var(--border);border-radius:4px;background-color:#fff;z-index:1;padding:.25rem 0;margin:0;max-height:350px;overflow-y:auto;box-shadow:#17494d26 0 20px 30px;animation:v-dd-menu-open-c63dbbfd var(--bezier-curve) 0s 1 normal;transition:display var(--bezier-curve)}.v-dd-option[data-v-c63dbbfd]{position:relative;display:flex;align-items:center;justify-content:space-between;cursor:pointer;padding:.5rem 1.25rem}.v-dd+ul.compact>li[data-v-c63dbbfd]{padding:.5rem 1.25rem}.v-dd+ul>li.parent-option[data-v-c63dbbfd]{border-bottom:var(--border);padding:.5rem .75rem}.v-dd+ul.compact>li.parent-option[data-v-c63dbbfd]{padding:.35rem .75rem}li.v-dd-option>svg[aria-labelledby=zd-check][data-v-c63dbbfd],li.v-dd-option>svg[aria-labelledby=zd-contains][data-v-c63dbbfd]{position:absolute}.v-dd-option>span[data-v-c63dbbfd]{padding:0 2rem;flex-grow:1;text-align:left}.v-dd-option.no-data[data-v-c63dbbfd],.v-dd-option[disabled=disabled][data-v-c63dbbfd]{-webkit-user-select:none;user-select:none;pointer-events:none;opacity:.7;cursor:not-allowed}.v-dd-option>svg[aria-labelledby=zd-down-pointer][data-v-c63dbbfd]{position:absolute;right:1rem;transform:rotate(-90deg)}.v-dd-option[data-v-c63dbbfd]:hover,.v-dd-option[data-v-c63dbbfd]:focus,.v-dd-option.active[data-v-c63dbbfd]{background-color:var(--background-color)}.v-dd-option.parent-option>svg[data-v-c63dbbfd]{left:.75rem;transform:rotate(90deg)}@keyframes v-dd-menu-open-c63dbbfd{0%{transform:translateY(2px)}to{transform:translateY(0)}}")),document.head.appendChild(d)}}catch(a){console.error("vite-plugin-css-injected-by-js",a)}})();
+import { openBlock as d, createElementBlock as p, createElementVNode as a, toDisplayString as h, defineComponent as C, resolveComponent as $, createCommentVNode as f, normalizeClass as O, createVNode as I, withDirectives as b, vModelText as D, vShow as M, Fragment as _, renderList as q, createBlock as g } from "vue";
 const P = (e, t) => {
-  const i = e.__vccOpts || e;
-  for (const [s, o] of t)
-    i[s] = o;
-  return i;
+  const o = e.__vccOpts || e;
+  for (const [s, i] of t)
+    o[s] = i;
+  return o;
 }, T = {
   props: {
     icon: {
@@ -38,7 +38,7 @@ const P = (e, t) => {
     };
   }
 }, L = T, z = ["color", "role", "aria-labelledby"], V = ["id"], B = ["innerHTML"];
-function A(e, t, i, s, o, c) {
+function A(e, t, o, s, i, c) {
   return d(), p("svg", {
     xmlns: "http://www.w3.org/2000/svg",
     color: e.color,
@@ -151,15 +151,15 @@ const E = /* @__PURE__ */ P(L, [["render", A]]), N = C({
     },
     selectedOptions: {
       handler(e, t) {
-        const i = t.map((o) => o.__identifier), s = e.map((o) => o.__identifier);
-        if (e.length !== t.length || !s.every((o) => i.includes(o)))
+        const o = t.map((i) => i.__identifier), s = e.map((i) => i.__identifier);
+        if (e.length !== t.length || !s.every((i) => o.includes(i)))
           try {
             this.$emit(
               "update:modelValue",
-              this.selectedOptions.map(({ __identifier: o, __selected: c, ...r }) => r)
+              this.selectedOptions.map(({ __identifier: i, __selected: c, ...r }) => r)
             );
-          } catch (o) {
-            console.log("Unknown Event ", o);
+          } catch (i) {
+            console.log("Unknown Event ", i);
           }
       },
       deep: !0
@@ -181,8 +181,8 @@ const E = /* @__PURE__ */ P(L, [["render", A]]), N = C({
     // TODO: Check if all options under parent can be selected - [LATER]
     constructCompData() {
       try {
-        const e = JSON.parse(JSON.stringify(this.options)), { formattedOptions: t, selectedOptions: i } = this.parseInputOptions(e);
-        if (this.ddOptions = t, this.selectedOptions = i, this.searchInput) {
+        const e = JSON.parse(JSON.stringify(this.options)), { formattedOptions: t, selectedOptions: o } = this.parseInputOptions(e);
+        if (this.ddOptions = t, this.selectedOptions = o, this.searchInput) {
           const s = this.filterMatchingOptions(this.searchInput, this.ddOptions);
           this.currentOptions = s, this.filteredOptions = s;
         } else
@@ -212,12 +212,12 @@ const E = /* @__PURE__ */ P(L, [["render", A]]), N = C({
       var t;
       (t = e.path || e.composedPath()) != null && t.includes(this.$refs.dropdown) || this.closeDropdownMenu();
     },
-    parseInputOptions(e = [], t = [], i = []) {
+    parseInputOptions(e = [], t = [], o = []) {
       return e.forEach((s) => {
         var c;
-        const o = [...t, s.label];
-        typeof s.value < "u" && o.push(s.value), s.__identifier = o.join("__"), (c = s.children) != null && c.length ? this.parseInputOptions(s.children, o, i) : s.selected && i.push(s);
-      }), { formattedOptions: e, selectedOptions: i };
+        const i = [...t, s.label];
+        typeof s.value < "u" && i.push(s.value), s.__identifier = i.join("__"), (c = s.children) != null && c.length ? this.parseInputOptions(s.children, i, o) : s.selected && o.push(s);
+      }), { formattedOptions: e, selectedOptions: o };
     },
     isOptionSelected(e) {
       return this.selectedOptions.findIndex((t) => t.__identifier === e.__identifier) > -1;
@@ -225,15 +225,15 @@ const E = /* @__PURE__ */ P(L, [["render", A]]), N = C({
     hasSelectedOptions(e) {
       return e.some(
         (t) => {
-          var i;
-          return (i = t.children) != null && i.length ? this.hasSelectedOptions(t.children) : this.isOptionSelected(t);
+          var o;
+          return (o = t.children) != null && o.length ? this.hasSelectedOptions(t.children) : this.isOptionSelected(t);
         }
       );
     },
     goToPreviousOptions(e = null, t = this.filteredOptions) {
-      var i;
+      var o;
       for (const s of t)
-        if ((i = s.children) != null && i.length) {
+        if ((o = s.children) != null && o.length) {
           if (s.__identifier === this.selectedParent.__identifier && s.children.length === this.currentOptions.length) {
             this.currentOptions = t, this.selectedParent = e;
             return;
@@ -243,27 +243,27 @@ const E = /* @__PURE__ */ P(L, [["render", A]]), N = C({
     },
     filterMatchingOptions(e, t) {
       var s;
-      let i = [];
-      for (const o of t) {
-        const { __identifier: c, __selected: r, ...u } = o, n = this.searchOptionMatcher(e, u);
-        if (typeof n == "boolean" ? n : o.label.toLowerCase().includes(e.toLowerCase()))
-          i.push(o);
-        else if ((s = o.children) != null && s.length) {
-          const l = this.filterMatchingOptions(e, o.children);
-          l.length && i.push({ ...o, children: l });
+      let o = [];
+      for (const i of t) {
+        const { __identifier: c, __selected: r, ...u } = i, n = this.searchOptionMatcher(e, u);
+        if (typeof n == "boolean" ? n : i.label.toLowerCase().includes(e.toLowerCase()))
+          o.push(i);
+        else if ((s = i.children) != null && s.length) {
+          const l = this.filterMatchingOptions(e, i.children);
+          l.length && o.push({ ...i, children: l });
         }
       }
-      return i;
+      return o;
     },
     selectOption(e) {
-      var i;
+      var o;
       const t = this.currentOptions[e];
-      if ((i = t.children) != null && i.length)
+      if ((o = t.children) != null && o.length)
         this.selectedParent = t, this.currentOptions = t.children;
       else if (t.__selected || this.isOptionSelected(t, this.selectedParent)) {
         if (this.multiple) {
           const s = this.selectedOptions.findIndex(
-            (o) => o.__identifier === t.__identifier
+            (i) => i.__identifier === t.__identifier
           );
           s > -1 && (this.selectedOptions = [
             ...this.selectedOptions.slice(0, s),
@@ -272,11 +272,20 @@ const E = /* @__PURE__ */ P(L, [["render", A]]), N = C({
         } else
           this.selectedOptions = [];
         this.currentOptions[e].__selected = !1;
-      } else
-        this.multiple ? this.selectedOptions = [...this.selectedOptions, t] : (this.selectedOptions = [t], this.closeDropdownMenu()), this.currentOptions[e].__selected = !0;
+      } else {
+        if (this.multiple)
+          this.selectedOptions = [...this.selectedOptions, t];
+        else {
+          const s = this.currentOptions.findIndex(
+            (i) => i.__selected === !0
+          );
+          s > -1 && (this.currentOptions[s].__selected = !1), this.selectedOptions = [t], this.closeDropdownMenu();
+        }
+        this.currentOptions[e].__selected = !0;
+      }
     },
     handleKeyDown(e) {
-      var i, s, o, c, r, u;
+      var o, s, i, c, r, u;
       const { key: t } = e;
       if (t === "Escape")
         this.closeDropdownMenu();
@@ -286,9 +295,9 @@ const E = /* @__PURE__ */ P(L, [["render", A]]), N = C({
             this.goToPreviousOptions(), this.selectedIndex = 0;
           else if (this.selectedIndex !== -1) {
             const n = this.selectedParent ? this.selectedIndex - 1 : this.selectedIndex;
-            if (!((i = this.currentOptions[n]) != null && i.disabled)) {
+            if (!((o = this.currentOptions[n]) != null && o.disabled)) {
               const l = (s = this.selectedParent) == null ? void 0 : s.label;
-              this.selectOption(n), l !== ((o = this.selectedParent) == null ? void 0 : o.label) && (this.selectedParent ? this.selectedIndex = 1 : this.selectedIndex = 0);
+              this.selectOption(n), l !== ((i = this.selectedParent) == null ? void 0 : i.label) && (this.selectedParent ? this.selectedIndex = 1 : this.selectedIndex = 0);
             }
           }
         }
@@ -316,7 +325,7 @@ const E = /* @__PURE__ */ P(L, [["render", A]]), N = C({
   class: "v-dd-option no-data",
   role: "option"
 }, W = ["id"], X = { class: "u-truncate label" }, Y = ["id", "onClick", "aria-selected", "disabled"], Z = { class: "u-truncate" };
-function x(e, t, i, s, o, c) {
+function x(e, t, o, s, i, c) {
   var u;
   const r = $("svg-icon");
   return d(), p("div", H, [
@@ -398,7 +407,7 @@ function x(e, t, i, s, o, c) {
         }),
         a("span", X, h(e.selectedParent.label), 1)
       ], 10, W)) : f("", !0),
-      (d(!0), p(q, null, _(e.currentOptions, (n, l) => {
+      (d(!0), p(_, null, q(e.currentOptions, (n, l) => {
         var v, w, y, k, S;
         return d(), p("li", {
           key: n.label + "__" + (n.value ?? l),
@@ -435,7 +444,7 @@ function x(e, t, i, s, o, c) {
     ])
   ], 512);
 }
-const ee = /* @__PURE__ */ P(N, [["render", x], ["__scopeId", "data-v-b72d9dbe"]]), te = {
+const ee = /* @__PURE__ */ P(N, [["render", x], ["__scopeId", "data-v-c63dbbfd"]]), te = {
   install(e, t) {
     e.component("VsAutocomplete", ee);
   }
--- vs-autocomplete/src/Dropdown.vue
+++ vs-autocomplete/src/Dropdown.vue
@@ -345,6 +345,10 @@ export default defineComponent({
           if (this.multiple) {
             this.selectedOptions = [...this.selectedOptions, option];
           } else {
+            const prevSelectedOptionIndex = this.currentOptions.findIndex(
+              option => option.__selected === true,
+            );
+            if (prevSelectedOptionIndex > -1) this.currentOptions[prevSelectedOptionIndex].__selected = false;
             this.selectedOptions = [option];
             this.closeDropdownMenu();
           }
